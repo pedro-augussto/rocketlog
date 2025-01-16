28a345e9eb373c19e8d9e78a15b48358fa671c38
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-16 00:10:07 -0300

    Criptografando Senha do Usuário

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt";
 import { z } from "zod";
 
-
 class UsersController {
-  create(request: Request, response: Response) {
-    const  bodySchema = z.object({
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
       name: z.string().trim().min(2),
       email: z.string().email(),
       password: z.string().min(6),
-    })
+    });
 
-    const {name, email, password} = bodySchema.parse(request.body)
+    const { name, email, password } = bodySchema.parse(request.body);
 
-    return response.json({ message: "ok" });
+    const hashedPassword = await hash(password, 8)
+
+    return response.json({ message: "ok", hashedPassword });
   }
 }
 
