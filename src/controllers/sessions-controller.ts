d6adaff93468b7d70f77d3c91148b48b510b489d
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-16 00:32:38 -0300

    Recuperando o Usuário e fazendo a validação

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,33 @@
 import { Request, response, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
 
 class SessionsController {
-    create(request: Request, response: Response){
-        return response.json({message: "ok"})
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+    if (!user) {
+      throw new AppError("Invalide email or password", 401);
     }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalide email or password", 401);
+    }
+
+    return response.json({ message: "ok" });
+  }
 }
 
-export {SessionsController}
+export { SessionsController };
