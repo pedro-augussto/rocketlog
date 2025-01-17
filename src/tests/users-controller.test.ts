d52eb501a5431ef02ea3795344fdf8dc2232ec17
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-17 01:09:10 -0300

    Testando Cadastro de Usuário com E-mail Indisponível

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -2,13 +2,12 @@ import request from "supertest";
 import { app } from "@/app";
 import { prisma } from "@/database/prisma";
 
-
 describe("UsersController", () => {
-    let user_id: string
+  let user_id: string;
 
-    afterAll(async () => {
-        await prisma.user.delete({where: {id: user_id}})
-    })
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } });
+  });
 
   it("should create a new user successfully", async () => {
     const response = await request(app).post("/users").send({
@@ -21,6 +20,17 @@ describe("UsersController", () => {
     expect(response.body).toHaveProperty("id");
     expect(response.body.name).toBe("Test User");
 
-    user_id = response.body.id
+    user_id = response.body.id;
+  });
+
+  it("should throw an error if user with same email already exist", async () => {
+    const response = await request(app).post("/users").send({
+      name: "TTest User",
+      email: "testuser@email.com",
+      password: "password123",
+    });
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("User with same email already exists")
   });
 });
