fa34451320700a0f6b8a664ef126a55b38374fee
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-17 00:02:00 -0300

    Cadastra Log

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,37 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    if (!delivery) {
+      throw new AppError("delivery not found", 401);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped", 401);
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
 
