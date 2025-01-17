95f6ddf48f4d8104fa130b0074aca6a139d9264c
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-17 01:06:07 -0300

    Removendo Usuário de Teste

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,15 @@
 import request from "supertest";
 import { app } from "@/app";
+import { prisma } from "@/database/prisma";
+
 
 describe("UsersController", () => {
+    let user_id: string
+
+    afterAll(async () => {
+        await prisma.user.delete({where: {id: user_id}})
+    })
+
   it("should create a new user successfully", async () => {
     const response = await request(app).post("/users").send({
       name: "Test User",
@@ -12,5 +20,7 @@ describe("UsersController", () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
     expect(response.body.name).toBe("Test User");
+
+    user_id = response.body.id
   });
 });
