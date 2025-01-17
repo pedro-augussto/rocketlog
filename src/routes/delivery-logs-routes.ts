21668761b55c1f4c8142684ce5c4308580630959
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-16 23:52:48 -0300

    Criando Controller e Rota de Logs da Entrega

---
+++ src/routes/delivery-logs-routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { DeliveryLogsController } from "@/controllers/delivery-logs-controller";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+
+const deliveryLogsRoutes = Router();
+const deliveryLogsController = new DeliveryLogsController();
+
+deliveryLogsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale"]),
+  deliveryLogsController.create
+);
+
+export { deliveryLogsRoutes };
