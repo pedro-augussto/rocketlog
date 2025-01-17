fb466213bdfca2ecc843e5db6faa1fed01a6aee5
Pedro Augusto <pa.severosilva@gmail.com>
2025-01-16 23:10:34 -0300

    Atualizando Status da Entrega

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
